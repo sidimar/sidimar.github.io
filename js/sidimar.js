03dc2fd0f7333822737931019a8eb8d93fc54a99
sidimar <sidimar@gmail.com>
2016-05-07 11:42:11 +0100

    modal close video and removed form

--- js/sidimar.js
+++ js/sidimar.js
@@ -15,17 +15,6 @@ $(function() {
     });
 });
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
@@ -34,6 +23,14 @@ $('body').scrollspy({
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+// Stop video when modal is closed
+$('#portfolioModalcase').on('hide.bs.modal', function(e) {
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
 });
 
 // Analytics
